ee90b036734b335ef236a93d5b807c3167759a95
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2017-01-03 14:26:19 +0100

    Added --dind option to the build script for docker-in-docker build mode.

--- build.sh
+++ build.sh
@@ -2,11 +2,28 @@
 
 THIS_DIR="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-docker run --rm -v "$THIS_DIR":/go/src/k8s.io/contrib/election \
-       -w /go/src/k8s.io/contrib/election golang:1.6 \
-       bash -c "rm -f ./server && make server && chown $EUID ./server" && \
-    echo "Successfully built election server" || \
-        exit
+cleanup() {
+    docker rm leader-elector-data
+}
+
+trap cleanup INT EXIT
+
+if [ "$1" == "--dind" ]; then
+    docker rm leader-elector-data || /bin/true
+    docker create -v /go/src/k8s.io/contrib/election --name leader-elector-data golang:1.6 /bin/true && \
+        docker cp "$THIS_DIR/." leader-elector-data:/go/src/k8s.io/contrib/election && \
+        docker run --rm --volumes-from leader-elector-data -w /go/src/k8s.io/contrib/election golang:1.6 \
+               /bin/bash -c "rm -f ./server && make server && chown $EUID ./server" && \
+        docker cp leader-elector-data:/go/src/k8s.io/contrib/election/server "$THIS_DIR/" && \
+        echo "Successfully built election server" || \
+            exit 1
+else
+    docker run --rm -v "$THIS_DIR":/go/src/k8s.io/contrib/election \
+           -w /go/src/k8s.io/contrib/election golang:1.6 \
+           bash -c "rm -f ./server && make server && chown $EUID ./server" && \
+        echo "Successfully built election server" || \
+            exit 1
+fi
 
 PREFIX=${PREFIX:-leader-elector}
 TAG=${TAG:-0.6}
